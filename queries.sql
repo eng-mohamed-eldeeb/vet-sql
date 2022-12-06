b3676149c98b5f317bdd4cc061e8bda855fb0127
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-12-06 23:14:19 +0200

    Update the queries

--- queries.sql
+++ queries.sql
@@ -39,4 +39,30 @@ where weight_kg between 10.5 and 17.3;
 
 
 -- set the species
+BEGIN;
 UPDATE animals SET species = 'unspecified';
+SELECT species from animals;
+ROLLBACK;
+SELECT species from animals;
+
+--  update the species
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS null;
+COMMIT;
+SELECT name, species from animals;
+
+-- delete the records
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+SELECT * from animals;
+--  Do the rest of aht is required
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_after_2022;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK to delete_after_2022;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
