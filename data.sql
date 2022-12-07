bb7909951882b6fe05b2de31a5425dc62adf724c
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-12-07 19:19:44 +0200

    Change the inserted data in the animals schema

--- data.sql
+++ data.sql
@@ -4,11 +4,12 @@ INSERT INTO animals VALUES (4, 'Pikachu' ,'2021-1-7', 1, false,15.04);
 INSERT INTO animals VALUES (5, 'Devimon' ,'2017-5-12', 5, true,11);
 
 -- new data
-INSERT INTO animals VALUES (DEFAULT, 'Charmander', '2020-02-08', 0, '0', -11);
-INSERT INTO animals VALUES (DEFAULT, 'Plantmon', '2021-11-15', 3, '0', -5.7);
-INSERT INTO animals VALUES (DEFAULT, 'Squirtle', '1993-04-02', 1, '1', -12.13);
-INSERT INTO animals VALUES (DEFAULT, 'Angemon', '2005-06-12', 1, '1', -45.13);
-INSERT INTO animals VALUES (DEFAULT, 'Boarmon', '2005-06-07', 7, '1', 20.4);
-INSERT INTO animals VALUES (DEFAULT, 'Blossom', '1998-10-13', 3, '1', 17);
-INSERT INTO animals VALUES (DEFAULT, 'Ditto', '2022-05-14', 4, '1', 22);
+
+INSERT INTO animals VALUES (6, 'Charmander', '2020-02-08', 0, '0', -11);
+INSERT INTO animals VALUES (7, 'Plantmon', '2021-11-15', 3, '0', -5.7);
+INSERT INTO animals VALUES (8, 'Squirtle', '1993-04-02', 1, '1', -12.13);
+INSERT INTO animals VALUES (9, 'Angemon', '2005-06-12', 1, '1', -45.13);
+INSERT INTO animals VALUES (10, 'Boarmon', '2005-06-07', 7, '1', 20.4);
+INSERT INTO animals VALUES (11, 'Blossom', '1998-10-13', 3, '1', 17);
+INSERT INTO animals VALUES (12, 'Ditto', '2022-05-14', 4, '1', 22);
 
